8b71af383c84921fff2d3173c84d795555fcc24f
Raghav-558 <jangraraghav9211@gmail.com>
2024-12-16 15:38:27 +0530

    add padding

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
   return (
     <div className='bg-deep-blue py-6 max-lg:px-4'>
       <div className='max-w-[1232px] mx-auto px-4'>
-        <div className='justify-between flex items-center'>
-          <p className='text-xs text-light-purple leading-7'>
+        <div className='justify-between flex items-center flex-wrap'>
+          <p className='text-xs text-light-purple leading-7 max-sm:w-full max-sm:text-center max-sm:leading-5'>
             ©{currentYear} KeySemantics GmbH | Ramsenburgweg 24, 9100 Herisau,
             Switzerland
           </p>
           <a
-            className='text-light-purple text-xs underline leading-7'
+            className='text-light-purple text-xs underline leading-7 max-sm:w-full max-sm:text-center'
             href='mailto:info@keysemantics.ch'
           >
             info@keysemantics.ch
